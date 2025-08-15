f2022be1510b6486c5d17fc27dd6f9d9f8419156
JinHyeon <tiemo0708@naver.com>
2025-08-15 19:09:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/medicarebe/MediCareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicarebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediCareBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
