c5bf59b7024e23e215ff62ce821b59bb3215ba98
baekjaehyuk <bjh010321@naver.com>
2025-08-15 22:35:57 +0900

    feat: 엔티티 설정

---
+++ src/main/java/com/medicarebe/video/domain/Video.java
@@ -0,0 +1,38 @@
+package com.medicarebe.video.domain;
+
+import com.medicarebe.core.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Video extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long id;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+}
